9d928f02356bbf529a4e0dde40ceeebe92f20dbd
MauroPresso <mauro.presso@gmail.com>
2025-01-01 19:12:51 -0300

    Agrega la documentación generada por Doxygen

---
+++ doc/html/search/all_1.js
@@ -0,0 +1,4 @@
+var searchData=
+[
+  ['im_0',['im',['../struct_complex.html#a845a40ec30e10dc6290c8d6786f02485',1,'Complex']]]
+];
---
+++ doc/html/search/variables_1.js
@@ -0,0 +1,4 @@
+var searchData=
+[
+  ['re_0',['re',['../struct_complex.html#af6855a730cba62c8ed20b0b8cd59a312',1,'Complex']]]
+];
